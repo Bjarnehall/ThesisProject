aab7022bb2a5798fd886ebf440322d442ec1639c
Jonas Bjarnehall <jonas.bjarnehall@outlook.com>
2026-02-23 18:48:07 +0100

    Added Dockerfile to run NGINX with HTTP/2 using openSSL for certificate along with bash scripts that build, run and test the server over HTTP/1.1 and HTTP/2

---
+++ konfiguration1/docker-texting-ground/http2/curlindex.bash
@@ -0,0 +1,13 @@
+
+echo "--------------------------------------------------------"
+echo "|-request index.html from localhost:8443 with HTTP/1.1-|"
+echo "--------------------------------------------------------"
+# Curl HTTP/1 localhost Yes SSL
+curl -ik --http1.1 https://localhost:8443
+
+echo
+echo "--------------------------------------------------------"
+echo "|-request index.html from localhost:8443 with HTTP/2---|"
+echo "--------------------------------------------------------"
+# Curl HTTP/2 localhost Yes SSL
+curl -ik --http2 https://localhost:8443
---
+++ konfiguration1/tech-stack.md
@@ -0,0 +1,6 @@
+
+#### Server
+
+NGINX
+
+Änvänds i stor utsräckning inom industrin, väldokumenterat och kompatibelt med HTTP/2 och HTTP/3.
